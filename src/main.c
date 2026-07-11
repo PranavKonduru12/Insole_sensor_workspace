d4be6e198c3975bccdd7c64c13d99bf748108f2d
PranavKonduru12 <pskonduru12+gh@gmail.com>
2026-07-11 11:58:52 -0700

    first commit. These changes were to configure the template to the adafruit featherboard

--- src/main.c
+++ src/main.c
@@ -1,106 +1,45 @@
-/*
- * Copyright (c) 2020 Libre Solar Technologies GmbH
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <inttypes.h>
-#include <stddef.h>
-#include <stdint.h>
-
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/adc.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/util.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 
-#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
-	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
-#error "No suitable devicetree overlay specified"
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "No led0 alias found in devicetree"
 #endif
 
-#define DT_SPEC_AND_COMMA_FOR_INPUTS(node_id, prop, idx) \
-	COND_CODE_1(DT_PHA_HAS_CELL_AT_IDX(node_id, prop, idx, input), \
-		    (ADC_DT_SPEC_GET_BY_IDX(node_id, idx),), ())
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
-/* Data of ADC io-channels specified in devicetree. */
-static const struct adc_dt_spec adc_channels[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
-			     DT_SPEC_AND_COMMA_FOR_INPUTS)
-};
+static void uart_print(const struct device *dev, const char *msg)
+{
+	while (*msg != '\0') {
+		uart_poll_out(dev, *msg);
+		msg++;
+	}
+}
 
 int main(void)
 {
-	int err;
-	uint32_t count = 0;
-	uint32_t buf;
-	struct adc_sequence sequence = {
-		.buffer = &buf,
-		/* buffer size in bytes, not number of samples */
-		.buffer_size = sizeof(buf),
-#if CONFIG_SAMPLE_ADC_CALIBRATE_REQUIRED
-		.calibrate = true,
-#endif
-	};
+	const struct device *uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
-	/* Configure channels individually prior to sampling. */
-	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		if (!adc_is_ready_dt(&adc_channels[i])) {
-			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
-			return 0;
-		}
-
-		err = adc_channel_setup_dt(&adc_channels[i]);
-		if (err < 0) {
-			printk("Could not setup channel #%d (%d)\n", i, err);
-			return 0;
-		}
+	if (!gpio_is_ready_dt(&led)) {
+		return 0;
 	}
 
-#ifndef CONFIG_COVERAGE
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
+
+	k_sleep(K_SECONDS(3));
+
 	while (1) {
-#else
-	for (int k = 0; k < 10; k++) {
-#endif
-		printk("ADC reading[%u]:\n", count++);
-		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-			int32_t val_mv;
+		gpio_pin_toggle_dt(&led);
 
-			printk("- %s, channel %d: ",
-			       adc_channels[i].dev->name,
-			       adc_channels[i].channel_id);
-
-			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
-
-			err = adc_read_dt(&adc_channels[i], &sequence);
-			if (err < 0) {
-				printk("Could not read (%d)\n", err);
-				continue;
-			}
-
-			/*
-			 * If using differential mode, the 16 bit value
-			 * in the ADC sample buffer should be a signed 2's
-			 * complement value.
-			 */
-			if (adc_channels[i].channel_cfg.differential) {
-				val_mv = (int32_t)((int16_t)buf);
-			} else {
-				val_mv = (int32_t)buf;
-			}
-			printk("%"PRId32, val_mv);
-			err = adc_raw_to_millivolts_dt(&adc_channels[i],
-						       &val_mv);
-			/* conversion to mV may not be supported, skip if not */
-			if (err < 0) {
-				printk(" (value in mV not available)\n");
-			} else {
-				printk(" = %"PRId32" mV\n", val_mv);
-			}
+		if (device_is_ready(uart_dev)) {
+			uart_print(uart_dev, "LED toggled. Main is running.\r\n");
 		}
 
-		k_sleep(K_MSEC(1000));
+		k_sleep(K_SECONDS(1));
 	}
+
 	return 0;
 }
